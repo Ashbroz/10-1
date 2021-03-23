954e3851aebacae4f502791981c2d76afe1e9937
Ashton Crowder <acrowder2@usf.edu>
2021-03-22 21:31:47 -0400

    Add project files.

---
+++ CD Account Test/SavingsAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test
+{
+    class SavingsAccount
+    {
+        // Fields
+        private string _accountNumber;
+        private decimal _internetRate;
+        private decimal _balance;
+
+        // Consutuctor
+        public SavingsAccount()
+        {
+            _accountNumber = "";
+            _internetRate = 0;
+            _balance = 0;
+        }
+
+        // Account Number property
+        public string AccountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value;  }
+        }
+
+        // Interest Rate property
+        public decimal InterestRate
+        {
+            get { return _internetRate; }
+            set { _internetRate = value; }
+        }
+
+        // Balance property
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+    }
+}
